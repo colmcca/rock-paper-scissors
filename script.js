df442798e407d2ac3cc53213bdb310ef35c0c5d0
Collin McCartney <collinpmccartney@gmail.com>
2024-05-07 21:37:52 -0400

    Added more javascript

--- script.js
+++ script.js
@@ -12,14 +12,37 @@ var userChoice;
 
 function startGame() {
     userChoice = window.prompt("Please choose R, P, or S");
-    if (userChoice == "R" || userChoice == "P" || userChoice == "S" || userChoice == "r" || userChoice == "p" || userChoice == "s") {
+    if (userChoice === "R" || userChoice === "P" || userChoice === "S" || userChoice === "r" || userChoice === "p" || userChoice === "s") {
 userChoice.toUpperCase();
+if (userChoice === randomComputerChoice) {
+    ties++;
+showScore();
+}
+else if (userChoice === "R" && randomComputerChoice === "S" || userChoice === "S" && randomComputerChoice === "P" || userChoice === "P" && randomComputerChoice === "R") {
+    wins++;
+    showScore();
+}
+else if (userChoice === "R" && randomComputerChoice === "P" || userChoice === "S" && randomComputerChoice === "R" || userChoice === "P" && randomComputerChoicecomputerChoice === "S") {
+    showScore();
+}
 
     }
 
     else  {
         window.alert("Please enter a valid choice")
 startGame();
+    }
+}
+
+function showScore() {
+    window.alert("You choose " + userChoice + " and the computer choose " + randomComputerChoice)
+    window.alert("Wins: " + wins + ", Losses: " + losses + ", Ties: " + ties);
+    var playAgain = window.confirm("Do you want to play again?");
+    if (playAgain) {
+        startGame();
+    }
+    else {
+        return;
     }
 }
 
