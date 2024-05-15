67e4a946b7bdd628105385bf1573bf3f491b2dc1
Collin McCartney <collinpmccartney@gmail.com>
2024-05-14 20:27:21 -0400

    Added README

---
+++ README.md
@@ -0,0 +1,21 @@
+# Rock, Paper, Scissors
+
+## Description
+
+This app is a simple JavaScript program that runs games of rock, paper, scissors. To play, type in r, p, or s and the computer will randomly generate a move back. Your wins, losses, and ties are tracked.
+
+## Installation
+
+N/A
+
+## Credits
+
+N/A
+
+## Link to Deployed Page
+
+https://colmcca.github.io/rock-paper-scissors/
+
+## License
+
+N/A
--- script.js
+++ script.js
@@ -6,18 +6,17 @@ var ties = 0;
 
 var computerChoice = ["R", "P", "S"]
 
-var randomComputerChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
-
 
 var userChoice;
 
 function startGame() {
+    var randomComputerChoice = computerChoice[Math.floor(Math.random() * computerChoice.length)];
     userChoice = window.prompt("Please choose R, P, or S");
     if (userChoice === "R" || userChoice === "P" || userChoice === "S" || userChoice === "r" || userChoice === "p" || userChoice === "s") {
 var capitalUserChoice = userChoice.toUpperCase();
 if (capitalUserChoice === randomComputerChoice) {
     ties++;
-    window.alert("You choose " + capitalUserChoice + " and the computer choose " + randomComputerChoice)
+    window.alert("You chose " + capitalUserChoice + " and the computer chose " + randomComputerChoice)
     window.alert("Wins: " + wins + ", Losses: " + losses + ", Ties: " + ties);
     var playAgain = window.confirm("Do you want to play again?");
     if (playAgain) {
